826c65af39ebb22d8daeb88b7e931455b4d156ef
Nehoray Genis <bvurth000100@gmail.com>
2022-06-29 20:17:45 +0300

    added keep interface and save to local storage

---
+++ apps/js/app-keep/cmps/keep-list.cmp.js
@@ -0,0 +1,34 @@
+import keepPreview from "./keep-preview.cmp.js"
+
+export default {
+    props: ["keeps"],
+    template: `
+        <section class="keep-list">
+            <ul>
+                <li v-for="keep in keeps" :key="keep.id" class="keep-preview-container" >
+                <keep-preview :keep="keep" />
+                <button @click="removeKeep(keep.id)"> X</button>
+
+                   <!-- <div class="actions">
+                       <router-link :to="'/keep/'+keep.id">Details</router-link>
+                       <router-link :to="'/keep/edit/'+keep.id">Edit</router-link>
+                       <button @click="remove(keep.id)">X</button>
+                    </div> -->
+                </li>
+            </ul>
+        </section>
+    `,
+    components: {
+        keepPreview,
+    },
+    methods: {
+        removeKeep(keepId){
+            this.$emit("remove", keepId)
+        }
+    
+        // select(keep) {
+        //     this.$emit("selected", keep)
+        // },
+    },
+    computed: {},
+}
--- apps/js/app-keep/views/keep-app.cmp.js
+++ apps/js/app-keep/views/keep-app.cmp.js
@@ -1,12 +1,70 @@
+import appHeader from "../cmps/app-header.cmp.js";
+import appFooter from "../cmps/app-footer.cmp.js";
+import keepAside from "../cmps/keep-aside.cmp.js";
+import { keepService } from "../services/keep-service.js";
+import keepList from "../cmps/keep-list.cmp.js";
+import keepAdd from "../cmps/keep-add.cmp.js";
 export default {
- template: `
- <h1>HELLO</h1>
+  template: `
+    <app-header />
+    <section class="keeps">
+      <keep-aside/>
+      <section class="main-content">
+        <keep-add/>
+        
+        <keep-list :keeps="keepsForDisplay" @remove="removekeep"/>
+      </section>
+
+    </section>
+    <app-footer /> 
 `,
-data() {
-return {};
-},
-created() {},
-methods: {},
-computed: {},
-unmounted() {},
+  data() {
+    return {
+      keeps:null,
+    };
+  },
+  components: {
+    appHeader,
+    appFooter,
+    keepAside,
+    keepList,
+    keepAdd,
+  },
+  created() {
+    keepService.query().then((keeps) => {
+      console.log(keeps);
+      this.keeps = keeps
+    });
+  },
+  methods: {
+    removekeep(id) {
+      keepService
+        .remove(id)
+        .then(() => {
+          console.log("Deleted successfully");
+          const idx = this.keeps.findIndex((keep) => keep.id === id);
+          this.keeps.splice(idx, 1);
+          eventBus.emit("show-msg", { txt: "Deleted successfully", type: "success" });
+        })
+        .catch((err) => {
+          console.log(err);
+          eventBus.emit("show-msg", { txt: "Error - try again later", type: "error" });
+        });
+    },
+  },
+  computed: {
+    keepsForDisplay() {
+      // if (!this.filterBy) return this.keeps;
+      // const filterKeeps = this.keeps.filter((keep) => {
+      //   return (
+      //     keep.listPrice.amount < this.filterBy.toPrice &&
+      //     keep.listPrice.amount > this.filterBy.fromPrice &&
+      //     keep.title.includes(this.filterBy.title)
+      //   );
+      // });
+      return this.keeps;
+
+    },
+  },
+  unmounted() {},
 };
