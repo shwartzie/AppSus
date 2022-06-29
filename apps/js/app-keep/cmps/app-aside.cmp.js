0d3f5a77081a6eb67b3d952fda16bec08e660cf6
Nehoray Genis <bvurth000100@gmail.com>
2022-06-29 13:52:22 +0300

    started keep app

---
+++ apps/js/app-keep/cmps/app-aside.cmp.js
@@ -0,0 +1,17 @@
+export default {
+  template: `
+  <a class="btn" href="#">Compose</a>
+  <a class="btn" href="#">starred</a>
+  <a class="btn" href="#">sent</a>
+  <a class="btn" href="#">drafts</a>
+  <a class="btn" href="#">inbox</a>
+
+`,
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+  computed: {},
+  unmounted() {},
+};
---
+++ apps/js/app-keep/views/keep-app.cmp.js
@@ -0,0 +1,12 @@
+export default {
+ template: `
+ <h1>HELLO</h1>
+`,
+data() {
+return {};
+},
+created() {},
+methods: {},
+computed: {},
+unmounted() {},
+};
