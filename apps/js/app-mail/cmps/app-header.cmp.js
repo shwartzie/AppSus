5971382cd6e2e6687ea2fd4d5aa680609c326916
shwartzie <ronnybusiness77@gmail.com>
2022-06-29 13:54:46 +0300

    added nav-bar features

--- apps/js/app-mail/cmps/app-header.cmp.js
+++ apps/js/app-mail/cmps/app-header.cmp.js
@@ -1,14 +1,21 @@
 export default {
     template: `
         <header class="app-header">
-            <div class="logo">
-                <h3>books</h3>
-            </div>
+            
             <nav class="nav-bar">
-                <router-link to="/">Home</router-link>|
-                <router-link to="/book">books</router-link>|
-                <router-link to="/about">About</router-link>|
-                <router-link to="/mail">Mail</router-link>|
+                <div class="hamburger">
+
+                </div>
+                <div class="logo">
+                    <img src="" alt="">
+                    <h3>Mail</h3>
+                </div>
+                <div class="apps-nav">
+                    <router-link to="/">Home</router-link>|
+                    <router-link to="/book">books</router-link>|
+                    <router-link to="/about">About</router-link>|
+                    <router-link to="/mail">Mail</router-link>|
+                </div>
             </nav>
         </header>
     
