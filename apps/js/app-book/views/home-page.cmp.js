afa10eeab99aa6576fc9592753189d72b235e718
Nehoray Genis <bvurth000100@gmail.com>
2022-06-29 11:48:45 +0300

    fixed pathes and locations"

---
+++ apps/js/app-book/views/home-page.cmp.js
@@ -0,0 +1,15 @@
+export default {
+    template: `
+ <section class="home-page app-main">
+    <h3>Home sweet home</h3>
+    <img src="img/homeScreen.gif" alt="" class="home-img">
+ </section>
+`,
+    data() {
+        return {};
+    },
+    created() { },
+    methods: {},
+    computed: {},
+    unmounted() { },
+};
